78f3fb0c73e3d7c222f55ce544dbb7adebf1631e
Paul Weber <pw@perfect-operations.com>
2023-10-28 12:39:26 +0200

    Added project metadata

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="sentrypy",
     version="0.1",
+    author="Paul Weber",
+    keywords="sentry api wrapper pythonic",
+    description="The pythonic API wrapper for Sentry.io",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/perfect-operations/sentrypy",
     packages=find_packages(),
     install_requires=[
         "requests",
@@ -11,6 +17,7 @@ setup(
         "dev": [
             "black",
             "jupyter",
+            "twine",
         ]
     },
     python_requires=">=3.8",
